a8421357be332fb242d6eae2199bd3639e287229
Naftali-B <nbudamba@gmail.com>
2024-05-03 03:43:28 +0300

    add

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    function concat that takes
+    a string str1 and a string str2 with annotations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Args:
+            str1: string
+            str1: string
+
+        Return:
+            Concatenated string
+    """
+
+    return (str1 + str2)
