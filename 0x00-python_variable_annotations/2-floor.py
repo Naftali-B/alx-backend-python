230eba81902abcb8492c45bd7a0d95fb31432edc
Naftali-B <nbudamba@gmail.com>
2024-05-03 03:46:39 +0300

    add

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    function floor which takes a float n
+    and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+        Args:
+            n: float number
+
+        Return:
+            Float number truncated to floor
+    """
+
+    return int(n)
